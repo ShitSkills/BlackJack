8386da7aef85bc3c02950ded25c203b11f08826d
GrosseTurtle <simon.temme@gmx.net>
2026-06-11 11:39:52 +0200

    Klasse Card fertig

--- Card.java
+++ Card.java
@@ -1,3 +1,27 @@
-public class Card {
-    
+public class Card
+{
+    private String suit;
+    private String rank;        // 2 - 10 B, Q, K, A
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public int getValue()
+    {
+        if (rank == "A") 
+            {
+                return 11;
+            }
+        if (rank == "K" || rank == "Q" || rank == "J") 
+            {
+                return 10;
+            }
+        if (Integer.parseInt(rank) >= 2 && Integer.parseInt(rank) <= 10)
+            {
+                return Integer.parseInt(rank);
+            }
+        return 0;
+    }
 }
