0f151051d7088dd199dd78b339d96b165bcbeed2
MrHawaiiToast <giegerich.maxi@web.de>
2026-06-15 09:51:27 +0200

    Dealer

--- Dealer.java
+++ Dealer.java
@@ -1,3 +1,36 @@
-public class Dealer extends Player {
+public class Dealer extends Player 
+{
+    public Dealer() 
+    {
+        super("Dealer");
+    }
+
+    public void play(Deck deck) 
+    {
+        while (getHandValue() < 17) 
+        {
+            takeCard(deck.drawCard());
+        }
+    }
+
+    public void showFirstCard() 
+    {
+        
+        System.out.println(getHand().getCard(0));
+    
+    }
+
+    public boolean beats(Player player) 
+    {
+        if (getHandValue() > 21) 
+        {
+            return false; 
+        }
+        if (player.getHandValue() > 21) 
+        {
+            return true; 
+        }
+        return getHandValue() >= player.getHandValue();
+    }
     
 }
